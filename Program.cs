20520a67e7325630a4a63aaf7ce2836da90c12f3
azolynsky <alexzolynsky@rocketmortgage.com>
2022-09-30 16:29:29 -0400

    updated app to use a data source instead of code to populate graph

--- Program.cs
+++ Program.cs
@@ -1,71 +1,84 @@
-﻿var graph = new Graph();
+﻿using Newtonsoft.Json;
 
-var theProducers = new MovieNode("The Producers");
-graph.addNode(theProducers);
-var bueller = new MovieNode("Ferris Bueller's Day Off");
-graph.addNode(bueller);
-var warGames = new MovieNode("War Games");
-graph.addNode(warGames);
+var graph = new Graph();
+List<ActorModel> actors;
 
-var theBirdcage = new MovieNode("The Birdcage");
-graph.addNode(theBirdcage);
+using (StreamReader r = new StreamReader("core-data.json"))
+{
+    string json = r.ReadToEnd();
+    actors = JsonConvert.DeserializeObject<List<ActorModel>>(json);
+}
 
-var theLionKing = new MovieNode("The Lion King");
-graph.addNode(theLionKing);
+foreach (var actor in actors)
+{
+    var actorNode = new ActorNode(actor.Name);
+    graph.addNode(actorNode);
 
-var mrsDoubtfire = new MovieNode("Mrs. Doubtfire");
+    foreach(var movie in actor.Movies)
+    {
+        var movieNode = (MovieNode) graph.findNode(movie);
+        if (movieNode == null){
+            movieNode = new MovieNode(movie);
+            graph.addNode(movieNode);
+        }
 
-var jtt = new ActorNode("Jonathan Taylor Thomas");
-graph.addNode(jtt);
-var matthewBroderick = new ActorNode("Matthew Broderick");
-graph.addNode(matthewBroderick);
-var nathanLane = new ActorNode("Nathan Lane");
-graph.addNode(nathanLane);
-var robinWilliams = new ActorNode("Robin Williams");
-graph.addNode(robinWilliams);
+        graph.addEdge(actorNode, movieNode);
+    }
+}
 
-graph.addEdge(jtt, theLionKing);
-graph.addEdge(matthewBroderick, theLionKing);
-graph.addEdge(matthewBroderick, theProducers);
-graph.addEdge(matthewBroderick, bueller);
-graph.addEdge(matthewBroderick, warGames);
-graph.addEdge(nathanLane, theBirdcage);
-graph.addEdge(nathanLane, theProducers);
-graph.addEdge(nathanLane, theLionKing);
-graph.addEdge(robinWilliams, theBirdcage);
-graph.addEdge(robinWilliams, mrsDoubtfire);
+var from = graph.findNode("Jonathan Taylor Thomas");
+var to = graph.findNode("Robin Williams");
 
-Console.WriteLine(string.Join(", ", graph.findShortestPath(jtt, robinWilliams).Select(n => n.name)));
+Console.WriteLine(string.Join(", ", graph.findShortestPath(from, to).Select(n => n.name)));
 
-class Graph {
+class ActorModel
+{
+    public string Name { get; set; }
+    public string[] Movies { get; set; }
+}
+
+class Graph
+{
     List<GraphNode> nodes;
 
-    public Graph(){
+    public Graph()
+    {
         this.nodes = new List<GraphNode>();
     }
 
-    public void addNode(GraphNode node){
+    public void addNode(GraphNode node)
+    {
         this.nodes.Add(node);
     }
 
-    public void addNeighbors(GraphNode node, List<GraphNode> neighbors){
+    public void addNeighbors(GraphNode node, List<GraphNode> neighbors)
+    {
         node.addNeighbors(neighbors);
     }
 
-    private void addEdge(GraphNode from, GraphNode to){
+    public GraphNode? findNode(string name)
+    {
+        return this.nodes.Find(n => n.name == name);
+    }
+
+    private void addEdge(GraphNode from, GraphNode to)
+    {
         from.addNeighbor(to);
         to.addNeighbor(from);
     }
 
-    public void addEdge(MovieNode from, ActorNode to){
+    public void addEdge(MovieNode from, ActorNode to)
+    {
         addEdge((GraphNode)from, (GraphNode)to);
     }
 
-    public void addEdge(ActorNode from, MovieNode to){
+    public void addEdge(ActorNode from, MovieNode to)
+    {
         addEdge((GraphNode)from, (GraphNode)to);
     }
 
-    public List<GraphNode> findShortestPath(GraphNode from, GraphNode to){
+    public List<GraphNode> findShortestPath(GraphNode from, GraphNode to)
+    {
         var queue = new Queue<GraphNode>();
         var visited = new HashSet<GraphNode>();
         var path = new Dictionary<GraphNode, GraphNode>();
@@ -73,15 +86,19 @@ class Graph {
         queue.Enqueue(from);
         visited.Add(from);
 
-        while(queue.Count > 0){
+        while (queue.Count > 0)
+        {
             var current = queue.Dequeue();
 
-            if(current == to){
+            if (current == to)
+            {
                 break;
             }
 
-            foreach(var neighbor in current.getNeighbors()){
-                if(!visited.Contains(neighbor)){
+            foreach (var neighbor in current.getNeighbors())
+            {
+                if (!visited.Contains(neighbor))
+                {
                     visited.Add(neighbor);
                     queue.Enqueue(neighbor);
                     path[neighbor] = current;
@@ -92,7 +109,8 @@ class Graph {
         var shortestPath = new List<GraphNode>();
         var currentPath = to;
 
-        while(currentPath != from){
+        while (currentPath != from)
+        {
             shortestPath.Add(currentPath);
             currentPath = path[currentPath];
         }
@@ -103,79 +121,98 @@ class Graph {
         return shortestPath;
     }
 
-    public List<GraphNode> getNodes(){
+    public List<GraphNode> getNodes()
+    {
         return this.nodes;
     }
 
-    public List<GraphNode> getNeighbors(GraphNode node) {
+    public List<GraphNode> getNeighbors(GraphNode node)
+    {
         return node.getNeighbors();
     }
 }
 
-interface IGraphNode {
+interface IGraphNode
+{
     public void addNeighbor(GraphNode node);
     public void addNeighbors(List<GraphNode> nodes);
 
     public List<GraphNode> getNeighbors();
 }
 
-abstract class GraphNode: IGraphNode {
+abstract class GraphNode : IGraphNode
+{
     public string name;
     public List<GraphEdge> edges;
 
-    public void addNeighbor(GraphNode node){
+    public void addNeighbor(GraphNode node)
+    {
         this.edges.Add(new GraphEdge(this, node));
     }
 
-    public void addNeighbors(List<GraphNode> nodes){
-        foreach(var node in nodes){
+    public void addNeighbors(List<GraphNode> nodes)
+    {
+        foreach (var node in nodes)
+        {
             this.addNeighbor(node);
         }
     }
 
-    public GraphNode(string name){
+    public GraphNode(string name)
+    {
         this.name = name;
         this.edges = new List<GraphEdge>();
     }
 
-    public List<GraphNode> getNeighbors() {
+    public List<GraphNode> getNeighbors()
+    {
         return edges.Select(e => e.to).ToList();
     }
 }
 
-class ActorNode : GraphNode {
-    public ActorNode(string name) : base(name) {}
+class ActorNode : GraphNode
+{
+    public ActorNode(string name) : base(name) { }
 
-    public void addNeighbor(MovieNode node){
+    public void addNeighbor(MovieNode node)
+    {
         this.edges.Add(new GraphEdge(this, node));
     }
 
-    public void addNeighbors(List<MovieNode> nodes){
-        foreach (var node in nodes) {
+    public void addNeighbors(List<MovieNode> nodes)
+    {
+        foreach (var node in nodes)
+        {
             this.addNeighbor(node);
         };
     }
 }
 
-class MovieNode : GraphNode {
-    public MovieNode(string name) : base(name) {}
+class MovieNode : GraphNode
+{
+    public MovieNode(string name) : base(name) { }
 
-    public void addNeighbor(ActorNode node){
+    public void addNeighbor(ActorNode node)
+    {
         this.edges.Add(new GraphEdge(this, node));
     }
 
-    public void addNeighbors(List<ActorNode> nodes){
-        foreach (var node in nodes) {
+    public void addNeighbors(List<ActorNode> nodes)
+    {
+        foreach (var node in nodes)
+        {
             this.addNeighbor(node);
         };
     }
 }
 
-class GraphEdge {
+class GraphEdge
+{
     GraphNode from;
     public GraphNode to;
 
-    public GraphEdge(GraphNode from, GraphNode to){
+    public GraphEdge(GraphNode from, GraphNode to)
+    {
         this.from = from;
         this.to = to;
     }
